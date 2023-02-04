746ff80c7e1a195b1cbee755855e2d1423e5e7eb
Hayk7JS7 <118001589+Hayk7JS7@users.noreply.github.com>
2023-02-05 02:10:05 +0400

    Add files via upload

--- PassowordGen.js
+++ PassowordGen.js
@@ -1,19 +1,14 @@
-// const textPasswordGen = document.querySelector(".text-1")
+// declaring variables
 const textPasswordLength = document.querySelector(".text-2")
-
 let upperCase = document.querySelector(".uppercase")
 const lowerCase = document.querySelector(".LowerCase")
 const symbol = document.querySelector(".Symbols")
 const number = document.querySelector(".Numbers")
 let h3 = document.querySelector("h3")
+let p = document.querySelector("p")
 
-let alpabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "J","a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// alpabet
-let sym = [",", ";", "'", "$", "%", "&", "*", "=", ".", "+", "_", "-", "?"]
-let symAlpabet = sym.concat(alpabet)
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let numAlpabet = num.concat(alpabet)
-function generator(arr, length = 50){
+// creating generator
+function generator(arr, length = 150){
     let str = "";    
     if(length <= 0){
         alert("Error")
@@ -24,53 +19,249 @@ function generator(arr, length = 50){
     }
     return str
 }
-let p = document.querySelector("p")
 
-// Button.addEventListener("click", function(){
-//         let p = document.querySelector("p")
-//         let generalPassword = generator(alpabet, textPasswordLength.value)
-//         p.innerHTML = generalPassword
+// arrays for generating password
+let alpabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i",  "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let upperCaseAlpabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "J",]
+let lowerCaseAlpabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let sym = [",", ";", "'", "$", "%", "&", "*", "=", ".", "+", "_", "-", "?"]
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-//         if(upperCase.addEventListener("click", function(){
-//             let upperCaseType = generator(alpabet, textPasswordLength.value).toUpperCase()
-//             p.innerHTML = upperCaseType
-//         })){}
-// })
 
+// changes with arrays
+let symAlpabet = sym.concat(alpabet)
+let upperCaseAlpSymbol = upperCaseAlpabet.concat(sym)
+let numAlpabet = num.concat(alpabet)
+let numUpperCase = upperCaseAlpabet.concat(num)
+let mixEvery = symAlpabet.concat(num)
+let lowerSymbol = lowerCaseAlpabet.concat(sym)
+let lowerCaseNum = lowerCaseAlpabet.concat(num)
+let symNumber = sym.concat(num)
+let numSymUp = numUpperCase.concat(sym)
+let lowerCaseSymNum = lowerSymbol.concat(num)
+// logic for selecting checkbox and changes
 textPasswordLength.addEventListener("change", (e) =>{
+    // upperCase logic
+    // A = upperCase                  B = lowerCase                 C = symbol            d = number 
     upperCase.addEventListener("click", () => {
-        if(upperCase.checked && lowerCase.checked){
-            alert("you cannot access lowercase and uppercase")
-         }
-        if(upperCase){}
-        if(upperCase.checked){
-        let upperCaseType = generator(alpabet, e.target.value).toUpperCase()
+        // A
+        if(upperCase.checked && !lowerCase.checked && !symbol.checked && !number.checked){
+
+            let upperCaseType = generator(alpabet, e.target.value).toUpperCase()
         p.innerHTML = upperCaseType
+         }
+        //  AB
+         else if(upperCase.checked && lowerCase.checked && !symbol.checked && !number.checked){
+            let upperLowerCases = generator(alpabet, e.target.value)
+            p.innerHTML = upperLowerCases
+         }
+        //  AC
+        else if(upperCase.checked && symbol.checked && !lowerCase.checked && !number.checked){
+            let symUpper = generator(upperCaseAlpSymbol, e.target.value)
+            p.innerHTML = symUpper
         }
-        
+        // AD
+        else if(upperCase.checked && number.checked && !lowerCase.checked && !symbol.checked){
+            let upperNum = generator(numUpperCase, e.target.value)
+            p.innerHTML = upperNum
+        }
+        // ACB === ABC
+         else if(upperCase.checked && lowerCase.checked && symbol.checked && !number.checked){
+            let symUpperLowerCase = generator(symAlpabet, e.target.value)
+            p.innerHTML = symUpperLowerCase
+         }
+        // ADB === ABD
+        else if(upperCase.checked && lowerCase.checked && !symbol.checked && number.checked){
+            let numUpperLowerCase = generator(numAlpabet, e.target.value)
+            p.innerHTML = numUpperLowerCase
+        }
+        // ACD
+        else if(upperCase.checked && !lowerCase.checked && symbol.checked && number.checked){
+            let numSymUpper = generator(numSymUp, e.target.value)
+            p.innerHTML = numSymUpper
+        }
+        // ABCD
+         else if(symbol.checked && upperCase.checked && lowerCase.checked && number.checked){
+            let everything = generator(mixEvery, e.target.value)
+            p.innerHTML = everything
+        }
     })
+    
+    // lowerCase logic
+    // A = upperCase                  B = lowerCase                 C = symbol            d = number 
     lowerCase.addEventListener("click", () => {
-        if(lowerCase.checked){
+        // B
+        if(lowerCase.checked && !upperCase.checked && !symbol.checked && !number.checked){
         let lowerCaseType = generator(alpabet, e.target.value).toLowerCase()
         p.innerHTML = lowerCaseType
         }
-        if(upperCase.checked && lowerCase.checked){
-            alert("you cannot access lowercase and uppercase")
-         }
-    })
-    symbol.addEventListener("click", () => {
-        if(symbol.checked){
-        let SymbolCaseType = generator(symAlpabet, e.target.value)
-        p.innerHTML = SymbolCaseType
+        // BA
+        else if(lowerCase.checked && upperCase.checked && !symbol.checked && !number.checked){
+            let upperLowerCases = generator(alpabet, e.target.value)
+            p.innerHTML = upperLowerCases
+        }
+        // BC
+        else if(lowerCase.checked && symbol.checked && !number.checked && !upperCase.checked){
+            let symLower = generator(lowerSymbol, e.target.value)
+            p.innerHTML = symLower
+        }
+        // BD
+        else if(lowerCase.checked && number.checked && !upperCase.checked && symbol.checked){
+            let lowerNum = generator(lowerCaseNum, e.target.value)
+            p.innerHTML = lowerNum
+        }
+        // BAC
+        else if(lowerCase.checked && upperCase.checked && symbol.checked && !number.checked){
+            let symUpperLowerCase = generator(symAlpabet, e.target.value)
+            p.innerHTML = symUpperLowerCase
+        }
+        // BAD === BDA
+        else if(lowerCase.checked && upperCase.checked && !symbol.checked && number.checked){
+            let numUpperLowerCase = generator(numAlpabet, e.target.value)
+            p.innerHTML = numUpperLowerCase
+        }
+        // BDC === BCD
+        else if(lowerCase.checked && !upperCase.checked && symbol.checked && number.checked){
+            let lowerSymNum = generator(lowerCaseSymNum, e.target.value)
+            p.innerHTML = lowerSymNum
+        }
+        // BACD
+        else if(symbol.checked && upperCase.checked && lowerCase.checked && number.checked){
+            let everything = generator(mixEvery, e.target.value)
+            p.innerHTML = everything
         }
     })
+    // symbol logic
+    // A = upperCase                  B = lowerCase                 C = symbol            d = number 
+    symbol.addEventListener("click", () => {
+        // C
+        if(symbol.checked && !upperCase.checked && !lowerCase.checked && !number.checked){
+        let SymbolCaseType = generator(sym, e.target.value)
+        p.innerHTML = SymbolCaseType
+        }
+        // CA
+        else if(symbol.checked && upperCase.checked && !lowerCase.checked && !number.checked){
+            let upperCaseAlp = generator(upperCaseAlpSymbol, e.target.value)
+            p.innerHTML = upperCaseAlp
+        }
+        // CD
+        else if(symbol.checked && !upperCase.checked && !lowerCase.checked && number.checked){
+            let symNumber = generator(symNumber, e.target.value)
+            p.innerHTML = symNumber
+        }
+        // CB
+        else if(symbol.checked && !upperCase.checked && lowerCase.checked && !number.checked){
+            let symLowerCases = generator(lowerSymbol, e.target.value)
+            p.innerHTML = symLowerCases
+        }
+        // CAB === CBA
+        else if(symbol.checked && upperCase.checked && lowerCase.checked && !number.checked){
+            let symUpperLower = generator(symAlpabet, e.target.value)
+            p.innerHTML = symUpperLower
+        }
+        // CAD === CDA
+        else if(symbol.checked && upperCase.checked && !lowerCase.checked && number.checked){
+            let symUpperNum = generator(numSymUp, e.target.value)
+            p.innerHTML = symUpperNum
+        }
+        // CDB === CBD
+        else if(symbol.checked && !upperCase.checked && lowerCase.checked && number.checked){
+            let symNumLower = generator(lowerCaseSymNum, e.target.value)
+            p.innerHTML = symNumLower
+        }
+        // CABD
+        else if(symbol.checked && upperCase.checked && lowerCase.checked && number.checked){
+            let everything = generator(mixEvery, e.target.value)
+            p.innerHTML = everything
+        }
+    })
+    // numberlogic
+    // A = upperCase                  B = lowerCase                 C = symbol            d = number 
     number.addEventListener("click", () => {
-        if(number.checked){
-        let NumberCaseType = generator(numAlpabet, e.target.value)
+        // D
+        if(number.checked && !upperCase.checked && !lowerCase.checked && !symbol.checked){
+        let NumberCaseType = generator(num, e.target.value)
         p.innerHTML = NumberCaseType
+        }
+        // DA
+        else if(number.checked && upperCase.checked && !lowerCase.checked && !symbol.checked){
+            let upperNum = generator(numUpperCase, e.target.value)
+            p.innerHTML = upperNum
+        }
+        // DB
+        else if(number.checked && !upperCase.checked && lowerCase.checked && !symbol.checked){
+            let lowerNum = generator(lowerCaseNum, e.target.value)
+            p.innerHTML = lowerNum
+        }
+        // DC
+        else if(number.checked && !upperCase.checked && !lowerCase.checked && symbol.checked){
+            let SymNum = generator(symNumber, e.target.value)
+            p.innerHTML = SymNum
+        }
+        // DAC === DCA
+        else if(number.checked && upperCase.checked && !lowerCase.checked && symbol.checked){
+            let numUpperSym = generator(numSymUp, e.target.value)
+            p.innerHTML = numUpperSym
+        }
+        // DAB === DBA
+        else if(number.checked && upperCase.checked && lowerCase.checked && !symbol.checked){
+            let alpNum = generator(numAlpabet, e.target.value)
+            p.innerHTML = alpNum
+        }
+        // DBC === DCB
+        else if(number.checked && !upperCase.checked && lowerCase.checked && symbol.checked){
+            let numLowerSym = generator(lowerCaseSymNum, e.target.value)
+            p.innerHTML = numLowerSym
+        }
+        // DABC
+        else if(number.checked && upperCase.checked && lowerCase.checked && symbol.checked){
+            let everything = generator(mixEvery, e.target.value)
+            p.innerHTML = everything
         }
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+// ex print abcd letters all ordered
+function order(letters){
+    let arr = letters.split("")
+    let str = ""
+   for(let i = 0; i < arr.length; i++){
+        console.log(arr[i])
+        str += arr[i]
+   }
+   return str
+}
+console.log(order("abcd"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const start = document.querySelector(".rangeStart")
@@ -94,3 +285,13 @@ button.addEventListener("click", function(){
     let rangePassword = generator(generalRange, lengthPass.value)
     h3Class.innerHTML = rangePassword
 })
+// Button.addEventListener("click", function(){
+//         let p = document.querySelector("p")
+//         let generalPassword = generator(alpabet, textPasswordLength.value)
+//         p.innerHTML = generalPassword
+
+//         if(upperCase.addEventListener("click", function(){
+//             let upperCaseType = generator(alpabet, textPasswordLength.value).toUpperCase()
+//             p.innerHTML = upperCaseType
+//         })){}
+// })
